bdc28753502a6c83fdf7b05229d2d63e49edb29a
Dhiraj57 <58966263+Dhiraj57@users.noreply.github.com>
2022-01-09 18:11:43 +0530

    Chest system implementation

---
+++ Source/ChestSystem/ChestSystemGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ChestSystemGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CHESTSYSTEM_API AChestSystemGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
